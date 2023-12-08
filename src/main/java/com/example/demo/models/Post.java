c781687eea36458a9762358001489415d5846dd3
Azad Kamarbandi <akamarbandi@unitedprint.rdb>
2023-12-09 00:05:33 +0100

    added Article form for inout

--- src/main/java/com/example/demo/models/Post.java
+++ src/main/java/com/example/demo/models/Post.java
@@ -7,6 +7,15 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Post {
+    public Post() {
+    }
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
